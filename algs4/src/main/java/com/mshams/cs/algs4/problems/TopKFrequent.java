2ec748be6576edc49c838be05ed5ca3505fc8f75
mehrshadshams <mehrshad.shams@gmail.com>
2019-03-25 11:22:31 -0400

    Add two problems

---
+++ algs4/src/main/java/com/mshams/cs/algs4/problems/TopKFrequent.java
@@ -0,0 +1,49 @@
+package com.mshams.cs.algs4.problems;
+
+import java.util.*;
+
+public class TopKFrequent {
+    public List<String> topKFrequent(String[] words, int k) {
+        PriorityQueue<Word> pq = new PriorityQueue<>();
+        Map<String, Word> map = new HashMap<>();
+        for (String w : words) {
+            Word word = map.getOrDefault(w, new Word(w, -1));
+            word.count++;
+
+            map.put(w, word);
+
+            pq.remove(word);
+            pq.add(word);
+
+
+        }
+
+        List<String> out = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            out.add(pq.poll().word);
+        }
+        return out;
+    }
+
+    private class Word implements Comparable<Word> {
+        String word;
+        int count;
+
+        Word(String w, int c) {
+            this.word = w;
+            this.count = c;
+        }
+
+        @Override
+        public int compareTo(Word w) {
+            if (count > w.count) {
+                return -1;
+            } else if (count < w.count) {
+                return 1;
+            }
+
+            return word.compareTo(w.word);
+        }
+    }
+
+}
